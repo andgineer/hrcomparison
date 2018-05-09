9774f2b2b4c543d94d97e8fe4aa52f39eb594b8c
masterAndrey <filbert@yandex.ru>
2018-05-09 20:35:50 +0300

    Chart function

--- chart.py
+++ chart.py
@@ -1,24 +1,44 @@
 from pylab import *
 import tcx
+import os.path
+import matplotlib.pyplot as plt
+from matplotlib.dates import DateFormatter
+import time
+from datetime import datetime
 
-date = '20180408_ski'
 
-data_garmin = tcx.TCXParser('/home/sorokin/Downloads/{}_garmin.tcx'.format(date))
-plot(data_garmin.time_values, data_garmin.hr_values, label='Garmin')
+def compare_chart(prefix, folder='.', save_to_file=False):
+    """
+    :param prefix: filename prefix
+    :param folder: folder with data files
+    :param save_to_file - if False(by default) shows the chart on screen.
+    if True then saves it into the file with name <prefix>.svg
+    """
 
-data_scosche = tcx.TCXParser('/home/sorokin/Downloads/{}_scosche.tcx'.format(date))
-plot(data_scosche.time_values, data_scosche.hr_values, label='Scosche')
+    fig, ax = plt.subplots()
+    now_timestamp = time.time()
+    utc_offset = datetime.fromtimestamp(now_timestamp) - datetime.utcfromtimestamp(now_timestamp)
+    ax.xaxis.set_major_formatter(DateFormatter('%H:%M'))
 
-# print(data_garmin.pace)
-#
-# plot(data_garmin.time_values, data_garmin.pace, label='Pace')
+    for file in os.listdir(folder):
+        if file.startswith(prefix):
+            data = tcx.TCXParser(os.path.join(folder, file))
+            for key, time_value in enumerate(data.time_values):
+                data.time_values[key] = time_value + utc_offset
+            sensor = file[len(prefix):].split('.')[0]
+            ax.plot(data.time_values, data.hr_values, label=sensor)
 
-xlabel('Time')
-ylabel('Heart rate')
-title('Heart rate')
-grid(True)
+    ax.grid(True)
+    fig.autofmt_xdate()
+    legend()
 
-legend()
-#show()
+    if save_to_file:
+        savefig(os.path.join(folder, prefix[:-1] + '.svg'))
+    else:
+        show()
 
-savefig('/home/sorokin/Downloads/{}_hr.svg'.format(date))
+
+if __name__ == "__main__":
+    #compare_chart('20180415_ski_', folder='/users/andrejsorokin/Downloads/')
+    compare_chart('20180506_roller_', folder='/users/andrejsorokin/Downloads/', save_to_file=True)
+    #compare_chart('20180509_run_', folder='/users/andrejsorokin/Downloads/', save_to_file=True)
